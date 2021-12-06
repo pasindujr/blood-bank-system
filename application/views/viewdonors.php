9374cb747a2aa6b4cbbaeb2d007df61e443fc0f2
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2021-12-06 22:40:36 +0530

    Added search and pagination functionalities to existing tables using DataTables.

--- application/views/viewdonors.php
+++ application/views/viewdonors.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css">
     <title>Kegalle Blood Bank - View Donors</title>
 </head>
 <body>
@@ -23,7 +24,7 @@ if (!($this->session->userdata('isStaffLoggedIn'))) {
     <hr>
     <h2 class="is-size-3 mb-3">View Donors 🔍</h2>
     <hr>
-    <table class="table table-dark table-hover">
+    <table id="donortable" class="table table-dark table-hover">
         <thead>
         <tr>
             <th scope="col">DonorID</th>
@@ -74,6 +75,15 @@ if (!($this->session->userdata('isStaffLoggedIn'))) {
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"></script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
+<script>$(document).ready(function () {
+        $('#donortable').DataTable();
+    });
+</script>
+
 <?php if ($this->session->flashdata('donorupdated')) { ?>
     <script>
         alertify.set('notifier', 'position', 'top-right');
--- application/views/viewstaff.php
+++ application/views/viewstaff.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css">
     <title>Kegalle Blood Bank - View Donors</title>
 </head>
 <body>
@@ -23,7 +24,7 @@ if (!($this->session->userdata('isAdminLoggedIn'))) {
     <hr>
     <h2 class="is-size-3 mb-3">View Staff 🔍</h2>
     <hr>
-    <table class="table table-dark table-hover">
+    <table id="stafftable" class="table table-dark table-hover">
         <thead>
         <tr>
             <th scope="col">Staff Name</th>
@@ -73,6 +74,14 @@ if (!($this->session->userdata('isAdminLoggedIn'))) {
         alertify.success("<?php echo $this->session->flashdata('staffupdated'); ?>")
     </script>
 <?php } ?>
+
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
+<script>$(document).ready(function () {
+        $('#stafftable').DataTable();
+    });
+</script>
 
 <script>
     function confirm(StaffUserName) {
