667fb4e549f44ade4211b1811dcd4610c8221f81
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2021-12-15 12:42:33 +0530

    Designed "The donation process" section

--- application/views/home.php
+++ application/views/home.php
@@ -112,6 +112,108 @@
     </div>
 </section>
 
+<!--The donation process-->
+
+<section id="donation-process">
+    <div class="container">
+        <h1 class="title text-center">THE DONATION PROCESS</h1>
+        <div class="row offset-1">
+            <div class="col-md-11 donation-process">
+                <p class="donation-process-title">The donation process</p>
+                <ul>
+                    <li>Giving blood is simple and it saves lives. When you give blood, it is collected so it can be
+                        used to treat someone else
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row offset-1">
+            <div class="col-md-11 donation-process">
+                <p class="donation-process-title">Before you give blood</p>
+                <ol>
+                    <li>Check you are able to give blood.</li>
+                    <li>Register your interest in being a donor.</li>
+                    <li>Follow the preparing to give blood recommendations.</li>
+                    <li>If you are unable to keep your donation appointment please try and give us at least 3 days'
+                        notice.
+                    </li>
+                </ol>
+            </div>
+        </div>
+
+        <div class="row offset-1">
+            <div class="col-md-11 donation-process">
+                <p class=donation-process-title>Welcome and preparation.</p>
+                <ul>
+                    <li>Remember to bring your completed donation safety check form with you, if you received one from
+                        us in the post. We will ask you to agree to our donor consent form which explains the importance
+                        of blood safety. It’s important to read that whenever you attend because advice does change. We
+                        will give you 500ml of fluid just before you give blood. Drinking this over about 5 minutes will
+                        help with your well-being during and after donation.
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row offset-1">
+            <div class="col-md-11 donation-process">
+                <p class=donation-process-title>Health screening</p>
+                <ul>
+                    <li>We make sure it is safe for you to donate, and that your blood donation is safe for a patient to
+                        receive.
+                        <ol>
+                            <li>
+                                We confirm your identity and ask you about the information on your donor health check
+                                form; a registered nurse may follow up if necessary.
+                            </li>
+                            <li>
+                                We test a drop of blood from your finger to check the iron levels (haemoglobin) in your
+                                blood.
+                            </li>
+                        </ol>
+                    </li>
+                    <li>If you are not able to donate we will explain why, and may ask you to make another
+                        appointment.
+                    </li>
+                    <li>If you are able to donate you will be asked to sit in a waiting area until you are called to a
+                        donation chair.
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row offset-1">
+            <div class="col-md-11 donation-process">
+                <p class=donation-process-title>Your blood donation</p>
+                <ul>
+                    <li>When you are comfortable on the chair we will ask you to confirm your name, address and date of
+                        birth.
+                    </li>
+                    <li>We examine your arm and place a cuff on your arm to maintain a small amount of pressure during
+                        donation (this does not measure blood pressure).
+                    </li>
+                    <li>We then examine your arm to find a suitable vein and clean it with an antiseptic sponge.
+                        <ol>
+                            <li>We will insert a needle in your arm which will collect your blood into a blood bag with
+                                your unique donor number.
+                            </li>
+                            <li>You should not feel any discomfort or pain. If you do, tell a member of staff.</li>
+                            <li>A scale weighs the blood and stops when you have donated 470ml (or just under a pint).
+                                This usually takes between 5-10 minutes.
+                            </li>
+                            <li>The needle will be removed and a sterile dressing applied to your arm.</li>
+                        </ol>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+
+
+</section>
+
 <?php include_once 'footer/footer.php' ?>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
