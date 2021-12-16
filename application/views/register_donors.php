e8e55e11a564dbd8c619e59f85984e1bae9af8ab
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2021-12-16 20:29:20 +0530

    Designed "donor register" frontend

--- application/views/register_donors.php
+++ application/views/register_donors.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assests/styles/register.css'); ?>">
     <title>Kegalle Blood Bank - Login</title>
 </head>
 <body>
@@ -19,80 +20,92 @@ if (!($this->session->userdata('isStaffLoggedIn'))) {
 ?>
 
 <div class="container">
-    <h2 class="is-size-3 mb-3">Register Donor</h2>
+
 
     <?php
     if (validation_errors()) { ?>
-        <div class="alert alert-danger" role="alert">
+        <div class="alert alert-danger mt-5" role="alert">
             <?php echo validation_errors(); ?>
         </div>
     <?php } ?>
 
     <?php echo form_open('Validate_Donors/register'); ?>
-    <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Donor Name</label>
-        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="eg - Pasindu Ruwandeniya"
-               name="donorname">
+
+<!--    ------------------------->
+
+    <div class="card mt-5">
+        <h5 class="card-header">Register Donors</h5>
+        <div class="card-body">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Donor Name</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="eg - Pasindu Ruwandeniya"
+                       name="donorname">
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Gender</label>
+
+                <?php $donorgender = array(
+                    '' => 'Select',
+                    'Male' => 'Male',
+                    'Female' => 'Female',
+                );
+                echo form_dropdown('donorgender', $donorgender, '', 'required="required"');
+                ?>
+
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Donor NIC</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="eg - 965438764V"
+                           name="donornic">
+                </div>
+
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Date of birth</label>
+                    <input type="date" class="form-control" id="exampleFormControlInput1" name="donordob">
+                </div>
+
+
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Weight</label>
+                    <input type="number" class="form-control" id="exampleFormControlInput1" name="donorweight"
+                           placeholder="65KG">
+                </div>
+
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Mobile Number</label>
+                    <input type="number" class="form-control" id="exampleFormControlInput1"
+                           placeholder="712345678 | Please don't use 0 at the beginning"
+                           name="donormobile">
+                </div>
+
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Blood Type</label>
+
+                    <?php $bloodtypes = array(
+                        '' => 'Select',
+                        'O-' => 'O-',
+                        'O+' => 'O+',
+                        'A-' => 'A-',
+                        'A+' => 'A+',
+                        'B-' => 'B-',
+                        'B+' => 'B+',
+                        'AB-' => 'AB-',
+                        'AB+' => 'AB+',
+                    );
+                    echo form_dropdown('bloodtypes', $bloodtypes, '', 'required="required"');
+                    ?>
+
+
+                    <div class="control ">
+                        <button type="submit" class="btn btn-primary"
+                        ">Submit</button>
+                    </div>
+        </div>
     </div>
 
-    <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Gender</label>
-
-        <?php $donorgender = array(
-            '' => 'Select',
-            'Male' => 'Male',
-            'Female' => 'Female',
-        );
-        echo form_dropdown('donorgender', $donorgender, '', 'required="required"');
-        ?>
-
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Donor NIC</label>
-            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="eg - 965438764V"
-                   name="donornic">
-        </div>
-
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Date of birth</label>
-            <input type="date" class="form-control" id="exampleFormControlInput1" name="donordob">
-        </div>
+<!--    ------------------------->
 
 
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Weight</label>
-            <input type="number" class="form-control" id="exampleFormControlInput1" name="donorweight"
-                   placeholder="65KG">
-        </div>
-
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Mobile Number</label>
-            <input type="number" class="form-control" id="exampleFormControlInput1"
-                   placeholder="712345678 | Please don't use 0 at the beginning"
-                   name="donormobile">
-        </div>
-
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Blood Type</label>
-
-            <?php $bloodtypes = array(
-                '' => 'Select',
-                'O-' => 'O-',
-                'O+' => 'O+',
-                'A-' => 'A-',
-                'A+' => 'A+',
-                'B-' => 'B-',
-                'B+' => 'B+',
-                'AB-' => 'AB-',
-                'AB+' => 'AB+',
-            );
-            echo form_dropdown('bloodtypes', $bloodtypes, '', 'required="required"');
-            ?>
-
-
-            <div class="control ">
-                <button type="submit" class="btn btn-primary"
-                ">Submit</button>
-            </div>
 
             <?php echo form_close(); ?>
 
